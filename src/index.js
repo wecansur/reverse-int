c71331048c1dfe27fe8faceeccd4f49c1b68720f
wecansur <vlkob95@gmail.com>
2022-06-24 16:00:15 +0800

    feat: make a solution fot the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const number = n < 0 ? String(n * -1) : String(n);
+  let result = ''
+
+  for (let i = 0; i < number.length; i++) {
+    result = `${number[i]}${result}`;
+  }
+  return Number(result);
 }
